c533563895a03caa560703c28ab21267d874937e
Zheyuan Yu <Zheyuan Yu@DESKTOP-5MEC14J>
2018-05-21 23:54:42 -0400

    My Final

---
+++ FinalExam/src/main/java/pkgApp/controller/RetirementController.java
@@ -0,0 +1,149 @@
+package pkgApp.controller;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import org.apache.poi.ss.formula.functions.FinanceLib;
+
+import com.sun.prism.paint.Color;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
+import javafx.scene.text.FontWeight;
+import javafx.scene.control.TextFormatter;
+import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
+
+import javafx.beans.value.*;
+
+import pkgApp.RetirementApp;
+import pkgCore.Retirement;
+
+public class RetirementController implements Initializable {
+
+	private RetirementApp mainApp = null;
+	@FXML
+	private TextField txtSaveEachMonth;
+	@FXML
+	private TextField txtYearsToWork;
+	@FXML
+	private TextField txtAnnualReturnWorking;
+	@FXML
+	private TextField txtWhatYouNeedToSave;
+	@FXML
+	private TextField txtYearsRetired;
+	@FXML
+	private TextField txtAnnualReturnRetired;
+	@FXML
+	private TextField txtRequiredIncome;
+	@FXML
+	private TextField txtMonthlySSI;
+
+	private HashMap<TextField, String> hmTextFieldRegEx = new HashMap<TextField, String>();
+
+	public RetirementApp getMainApp() {
+		return mainApp;
+	}
+
+	public void setMainApp(RetirementApp mainApp) {
+		this.mainApp = mainApp;
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		// Adding an entry in the hashmap for each TextField control I want to validate
+		// with a regular expression
+		// "\\d*?" - means any decimal number
+		// "\\d*(\\.\\d*)?" means any decimal, then optionally a period (.), then
+		// decmial
+		hmTextFieldRegEx.put(txtYearsToWork, "\\d*?");
+		hmTextFieldRegEx.put(txtAnnualReturnWorking, "\\d*(\\.\\d*)?");
+		hmTextFieldRegEx.put(txtYearsRetired, "\\d*?");
+		hmTextFieldRegEx.put(txtAnnualReturnRetired, "\\d*(\\.\\d*)?");
+		hmTextFieldRegEx.put(txtRequiredIncome, "\\d*?");
+		hmTextFieldRegEx.put(txtMonthlySSI, "\\d*?");
+
+		// Check out these pages (how to validate controls):
+		// https://stackoverflow.com/questions/30935279/javafx-input-validation-textfield
+		// https://stackoverflow.com/questions/40485521/javafx-textfield-validation-decimal-value?rq=1
+		// https://stackoverflow.com/questions/8381374/how-to-implement-a-numberfield-in-javafx-2-0
+		// There are some examples on how to validate / check format
+
+		Iterator it = hmTextFieldRegEx.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry) it.next();
+			TextField txtField = (TextField) pair.getKey();
+			String strRegEx = (String) pair.getValue();
+
+			txtField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+				@Override
+				public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue,
+						Boolean newPropertyValue) {
+					// If newPropertyValue = true, then the field HAS FOCUS
+					// If newPropertyValue = false, then field HAS LOST FOCUS
+					if (!newPropertyValue) {
+						if (!txtField.getText().matches(strRegEx)) {
+							txtField.setText("");
+							txtField.requestFocus();
+						}
+					}
+				}
+			});
+		}
+
+		//
+		// TODO: Validate Working Annual Return %, accept only numbers and decimals
+		// TODO: Validate Years retired, accepted only decimals
+		// TODO: Validate Retired Annual Return %, accept only numbers and deciamls
+		// TODO: Validate Required Income, accept only decimals
+		// TODO: Validate Monthly SSI, accept only decimals
+	}
+
+	@FXML
+	public void btnClear(ActionEvent event) {
+		System.out.println("Clear pressed");
+
+		// disable read-only controls
+		txtSaveEachMonth.setDisable(true);
+		txtWhatYouNeedToSave.setDisable(true);
+
+		// Clear, enable txtYearsToWork
+		txtYearsToWork.clear();
+		txtYearsToWork.setDisable(false);
+
+		// TODO: Clear, enable the rest of the input controls. Hint! You already have a
+		// HashMap of all the input controls....!!!!
+		txtAnnualReturnWorking.clear();
+		txtAnnualReturnWorking.setDisable(false);
+		txtYearsRetired.clear();
+		txtYearsRetired.setDisable(false);
+		txtAnnualReturnRetired.clear();
+		txtAnnualReturnRetired.setDisable(false);
+		txtRequiredIncome.clear();
+		txtRequiredIncome.setDisable(false);
+		txtMonthlySSI.clear();
+		txtMonthlySSI.setDisable(false);
+	}
+
+	@FXML
+	public void btnCalculate() {
+
+		System.out.println("calculating");
+
+		txtSaveEachMonth.setDisable(false);
+		txtWhatYouNeedToSave.setDisable(false);
+
+		// TODO: Calculate txtWhatYouNeedToSave value...
+		// TODO: Then calculate txtSaveEachMonth, using amount from txtWhatYouNeedToSave
+		// as input
+	}
+}
---
+++ FinalExam/src/main/java/pkgCore/Retirement.java
@@ -0,0 +1,107 @@
+package pkgCore;
+
+import org.apache.poi.ss.formula.functions.FinanceLib;
+
+public class Retirement {
+
+	private int iYearsToWork;
+	private double dAnnualReturnWorking;
+	private int iYearsRetired;
+	private double dAnnualReturnRetired;
+	private double dRequiredIncome;
+	private double dMonthlySSI;
+
+	public Retirement(int iYearsToWork, double dAnnualReturnWorking, int iYearsRetired, double dAnnualReturnRetired,
+			double dRequiredIncome, double dMonthlySSI) {
+		super();
+		this.iYearsToWork = iYearsToWork;
+		this.dAnnualReturnWorking = dAnnualReturnWorking;
+		this.iYearsRetired = iYearsRetired;
+		this.dAnnualReturnRetired = dAnnualReturnRetired;
+		this.dRequiredIncome = dRequiredIncome;
+		this.dMonthlySSI = dMonthlySSI;
+	}
+
+	public double MonthlySavings() {
+
+		//TODO: Calculate AmountToSave
+		double pmt = 0; // <-- this should be fixed to calculate the real pmt
+		pmt = FinanceLib.pmt(dAnnualReturnWorking/12, iYearsToWork*12, 0, TotalAmountToSave(), false);
+		return pmt;
+	}
+
+	public double TotalAmountToSave() {
+		
+		//TODO: Calculate the Total Amount Requried to save
+		double pv = 0;
+		pv = FinanceLib.pv(dAnnualReturnRetired/12, iYearsRetired*12, dRequiredIncome - dMonthlySSI, 0, false);
+		//	Hint: Here's how to round a number: pv = Math.round(pv * 100.0) / 100.0;
+		return pv;
+	}
+
+	public static double PMT(double r, double n, double p, double f, boolean t) {
+		//	r = Rate
+		//	n = number of payments
+		//	p = present value
+		//	f = future value
+		//	t = boolean... when interest is calculated... we're going to use FALSE
+		return FinanceLib.pmt(r, n, p, f, t);
+	}
+
+	public static double PV(double r, double n, double y, double f, boolean t) {
+		//	r = Rate.  7% would be expressed as...  0.07 / 12
+		//	n = Number of payments.  Five years would be expressed as...  5 * 12...  or 60
+		//	y = PMT amount
+		//	f = Future value
+		//	t = boolean... when interest is calculated... we're going to use FALSE
+		return FinanceLib.pv(r, n, y, f, t);
+	}
+
+	public int getiYearsToWork() {
+		return iYearsToWork;
+	}
+
+	public void setiYearsToWork(int iYearsToWork) {
+		this.iYearsToWork = iYearsToWork;
+	}
+
+	public double getdAnnualReturnWorking() {
+		return dAnnualReturnWorking;
+	}
+
+	public void setdAnnualReturnWorking(double dAnnualReturnWorking) {
+		this.dAnnualReturnWorking = dAnnualReturnWorking;
+	}
+
+	public int getiYearsRetired() {
+		return iYearsRetired;
+	}
+
+	public void setiYearsRetired(int iYearsRetired) {
+		this.iYearsRetired = iYearsRetired;
+	}
+
+	public double getdAnnualReturnRetired() {
+		return dAnnualReturnRetired;
+	}
+
+	public void setdAnnualReturnRetired(double dAnnualReturnRetired) {
+		this.dAnnualReturnRetired = dAnnualReturnRetired;
+	}
+
+	public double getdRequiredIncome() {
+		return dRequiredIncome;
+	}
+
+	public void setdRequiredIncome(double dRequiredIncome) {
+		this.dRequiredIncome = dRequiredIncome;
+	}
+
+	public double getdMonthlySSI() {
+		return dMonthlySSI;
+	}
+
+	public void setdMonthlySSI(double dMonthlySSI) {
+		this.dMonthlySSI = dMonthlySSI;
+	}
+}
